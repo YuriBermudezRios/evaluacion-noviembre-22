a6fc2ce6c61a6bd00c2237e8ba33736d5f6bd015
YuriBermudezRios <yuri2894@gmail.com>
2022-11-10 17:52:13 -0500

    Examen Yuri

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import postsNotes from './components/Posteos';
+import statusCards from './componentes/Estatus';
+import {useState} from 'react';
+import Estatus from './components/Estatus';
+import Posteos from './components/Posteos';
+
+// El componente App es el padre de:
+// - Estatus
+// - Posteos
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de likes.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Post.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos para que manipulen o lean su estado.
+
+function App(){
+  const [likes,setLikes] = useState(0)
+  const [likePost,setLikePost] = useState([])
+
+  function addLikes(){
+    setLikes(likes+1)
+  } 
+
+  return (
+    <div className='App'>
+      <Estatus quantityLikes={likes}/>
+      <Posteos addLikes={addLikes}/>
+    </div>
+  );
+
+
+}
+
+export default App;
